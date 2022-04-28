8aeb6d8dcb6d0b8d6107693881c3d5ec30ed1e4c
Wei Zhang <zhang.wei8@northeastern.edu>
2022-04-27 22:51:19 -0400

    Update bookmark-screen-test

--- src/tests/bookmark-screen.test.js
+++ src/tests/bookmark-screen.test.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {act, create} from "react-test-renderer";
 import bookmarkedTuits from "../components/bookmarks/my-bookmarks"
+import {render, screen} from "@testing-library/react";
+import {HashRouter} from "react-router-dom";
+import Bookmarks from "../components/bookmarks";
 import MyBookmarks from "../components/bookmarks/my-bookmarks";
 
 
 const MOCKED_TUITS = [
-    {tuit:"alice's tuit", postBy:"alice", _id:"123", stats:{likes: 12, dislikes: 21, bookmarks: 22}},
     {tuit:"bob's tuit", postBy: "bob", _id: "345",  stats:{likes: 34, dislikes: 43, bookmarks: 23}}
 ]
 
@@ -13,18 +15,43 @@ console.error = () => {};
 
 test('renders bookmarks tab on bookmarks', () => {
     let bookmark;
+console.error = () => {
+};
+test('render bookmark screen', async () => {
     // eslint-disable-next-line testing-library/no-unnecessary-act
+    await act(async () => render(
+        <HashRouter>
+            <Bookmarks/>
+        </HashRouter>
+    ));
+
+    const bookmarksTab = screen.getByText(/Bookmark/i);
+    expect(bookmarksTab).toBeInTheDocument();
+})
+
+
+test('bookmarked tuit renders under bookmark screen', async () => {
+    let bookmarks
     act(() => {
         bookmark = create(
+        bookmarks = create(
             <MyBookmarks
                 tuits = {MOCKED_TUITS}/>
+                Tuits={MOCKED_TUITS}/>
         )
     });
+    })
 
     const root = bookmark.root;
     const ttrTuits = root.findByProps({className:"ttr-tuit"});
     expect(ttrTuits.length).toEqual(bookmarkedTuits.length);
     ttrTuits.forEach((ttrTuits, ndx) => {
         expect(ttrTuits.props.children).toBe(bookmarkedTuits[ndx].tuit);
+    const root = bookmarks.root
+    // eslint-disable-next-line testing-library/await-async-query
+    const ttrTuits = root.findAllByProps({className: 'ttr-tuit'})
+    ttrTuits.forEach((ttrTuit, ndx) => {
+        // eslint-disable-next-line testing-library/no-node-access
+        expect(ttrTuit.props.children[0]).toBe(MOCKED_TUITS[ndx].tuit)
     })
-})
+}) 
